d67ea65877bdc943ef7d6c1d52b8fb307131f14c
Sufia Bano <124654106+sufirang@users.noreply.github.com>
2023-04-20 10:10:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 function getFormvalue() {
     //Write your code here
 
+  const firstName = document.getElementById("fname").value;
+  const lastName = document.getElementById("lname").value;
+  alert("First Name: " + firstName + "\nLast Name: " + lastName);
+
+	
+
 }
